45185866fc28a1384e440a1bc50ad98785dbc055
bukarinevgeni <veliknon@gmail.com>
2024-04-08 15:28:26 +0300

    update structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
-import { getWeatherData } from './services/api';
+import WeatherApp from './components/WeatherApp';
 
-function WeatherApp() {
-  let { city } = useParams();
-  let [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    
-    const fetchData = async () => {
-      try {
-        const data = await getWeatherData(city);
-        setWeatherData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-    
-  }, []); // Added city to dependency array to re-fetch data when city changes
-
-  return (
-    <div className="App">
-      <main>
-        <h1>Weather App</h1>
-        <p>Weather data for {city}: {weatherData ? JSON.stringify(weatherData) : 'Loading...'}</p>
-      </main>
-    </div>
-  );
-}
-
-function AppWrapper() {
+function App() {
   return (
     <Router>
       <Routes>
@@ -42,4 +12,4 @@ function AppWrapper() {
   );
 }
 
-export default AppWrapper;
+export default App;
