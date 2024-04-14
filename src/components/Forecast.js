7684c5d194caa7fbe23510afb4ec0474c5dad14c
bukarinevgeni <veliknon@gmail.com>
2024-04-14 18:58:29 +0300

    add tetst modal

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,13 +1,20 @@
 import weatherCodeDescription from '../utils/weatherCodeDescription';
 import { TimeFormat, DateFormat } from '../services/DateTime';
+import { useState } from 'react';
+import MoodalBased from './ModalBased';
 import '../css/Forecast.css';
 
 
 function Forecast({daily}){
 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   function tableRows(daily){
     return daily.sunrise.map((date, index) => (
-      <tr key={date}>
+      <tr key={index} onClick={() => dailyWeather(index)}>
         <td scope="row">{DateFormat(date)}</td>
         <td>{daily.temperature_max[index]}°C</td>
         <td>{daily.temperature_min[index]}°C</td>
@@ -18,8 +25,18 @@ function Forecast({daily}){
     ))
   }
 
+  function dailyWeather(index){
+    console.log('Daily weather', index);
+    const hourly= daily.hourly[index];
+    console.log('Hourly weather', hourly, Object.keys(hourly));
+    
+
+  }
+
+
   return (
     <main className='forecast-block'>
+      <MoodalBased show={show} handleClose={handleClose} heading='Hourly forecast' body='Hourly forecast goes here'/>
       <h2 className='forecast-title'>Daily forecast</h2>
       <div className="table-responsive">
         {/* table-striped-primary */}
