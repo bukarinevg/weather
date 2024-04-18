33ab715fc7e9807f071af7b4c1cface2b0424254
bukarinevgeni <veliknon@gmail.com>
2024-04-18 04:25:43 +0300

    header changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ function Header({setLocation}){
     return (
         <Navbar variant="dark">
             <Container className="d-flex justify-content-between align-items-center">
-                <Navbar.Brand href="#home">Weather</Navbar.Brand>
+                <Navbar.Brand href="/">Weather</Navbar.Brand>
                 <Form className="d-flex justify-content-right"  onSubmit={handleFormSubmit}>
                     <FormControl type="text" placeholder="Search location..." className="" onChange={handleInputChange} />
                     <Button className='ms-2 btn ' variant="outline-info" type="submit">Search</Button>
