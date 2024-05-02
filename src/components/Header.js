79227230f833198f5dafa2429cdd77d42b49cd09
Evgenii  Bukarin <48152501+bukarinevg@users.noreply.github.com>
2024-05-03 01:26:48 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,19 +17,24 @@ function Header({location, setLocation}){
         setInput(event.target.value);
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setLocation(input);
+    }
+
     
     return (
         <Navbar variant="dark">
             <Container className="d-flex justify-content-between align-items-center">
                 <Navbar.Brand href="/weather">Weather</Navbar.Brand>
-                <Form className="d-flex justify-content-right">
+                <Form className="d-flex justify-content-right" onSubmit={handleSubmit}>
                     <FormControl 
                         value={input }
                         type="text" 
                         placeholder="Search location..." 
                         onChange={handleInputChange} 
                     />
-                    <Button className='ms-2 btn ' variant="outline-info" type="submit">Search</Button>
+                    {/* <Button className='ms-2 btn ' variant="outline-info" type="submit">Search</Button> */}
                 </Form>
             </Container>
         </Navbar>
